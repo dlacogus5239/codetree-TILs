bc735c8fcd4834fa35e04eb3440bd7f81a9f6809
채현 <71282435+dlacogus5239@users.noreply.github.com>
2024-01-03 13:37:34 +0900

    Title: 데이터 교환, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240103/데이터 교환/exchange-data.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        int tmp = b;
+        int tmpc = c;
+        b = a;
+        c = tmp;
+        a = tmpc;
+        
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
