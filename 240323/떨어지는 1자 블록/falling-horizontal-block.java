84b8329c3481b9cddf5cb224c1709fd2876dc027
채현 <71282435+dlacogus5239@users.noreply.github.com>
2024-03-23 14:56:42 +0900

    Title: 떨어지는 1자 블록, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240323/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int width = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken()) - 1;
+
+		int[][] map = new int[N][N];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		Drop: for (int i = 0; i < N; i++) {
+			for (int j = start; j < start + width; j++) {
+				if (map[i][j] != 0) {
+					for (int k = start; k < start + width; k++) {
+
+						map[i - 1][k] = 1;
+					}
+					break Drop;
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
