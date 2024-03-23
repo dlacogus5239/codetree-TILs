669d2e3b4c05f7e9a881e3e908902c043f3a2a3d
채현 <71282435+dlacogus5239@users.noreply.github.com>
2024-03-23 15:52:07 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 3080ms, Memory: 8MB, Status: Time Limit Exceed - CodeTree

---
+++ 240323/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static char[][] map;
+
+	// 우하좌상(Clockwise)
+	static int[] dr = { 0, 1, 0, -1 };
+	static int[] dc = { 1, 0, -1, 0 };
+	static int r, c;
+	static int dir = 0;
+
+	static final int INF = 10_001;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		r = Integer.parseInt(st.nextToken()) - 1;
+		c = Integer.parseInt(st.nextToken()) - 1;
+		map = new char[N][N];
+		for (int i = 0; i < N; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+		Move();
+	}
+
+	public static void Move() {
+		int time = 0;
+		while (true) {
+//			System.out.println("[" + r + ", " + c + "]");
+
+			int nr = r + dr[dir];
+			int nc = c + dc[dir];
+			if (!isIn(nr, nc)) {// 탈출
+				time++;
+				break;
+			}
+
+			if (map[nr][nc] == '#') { // 벽인 경우
+				dir = (dir + 3) % 4; // 반시계방향으로 TURN
+				continue;
+			} else if (map[nr][nc] == '.') { // 빈공간인경우
+				// 오른쪽 검사
+				int tmpR = nr + dr[(dir + 1) % 4];
+				int tmpC = nc + dc[(dir + 1) % 4];
+
+				if (map[tmpR][tmpC] == '#') {// 오른쪽에 벽이 있으면
+					// 이동
+					r = nr;
+					c = nc;
+					time++;
+				} else { // 벽이 없는 경우
+					dir = (dir + 1) % 4; // 방향 바꿔주고
+					r = nr + dr[dir]; // 앞에 이동한거 더해서
+					c = nc + dc[dir]; // 이동 한번 더
+
+					time += 2;
+				}
+			}
+
+			if (time >= INF) {
+				time = -1;
+				break;
+			}
+
+		}
+
+		System.out.println(time);
+
+	}
+
+	public static boolean isIn(int r, int c) {
+		return !(r < 0 || c < 0 || r >= N || c >= N);
+	}
+}
